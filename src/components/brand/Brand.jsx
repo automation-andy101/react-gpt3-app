9a195f7a361f42a1d68ff74dcb5f82810b31588b
Andy Short <automation.andy101@gmail.com>
2023-04-08 10:05:24 +0100

    Added whatgpt3 and brand components

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './brand.css';
 
+import { Google, Slack, Atlassian, Dropbox, Shopify } from './imports';
+
 
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className='gpt3__brand section__padding'>
+      <div className='gpt3__brand'>
+        <img src={Google} alt='google' />
+      </div>
+      <div className='gpt3__brand'>
+        <img src={Slack} alt='slack' />
+      </div>
+      <div className='gpt3__brand'>
+        <img src={Atlassian} alt='atlassian' />
+      </div>
+      <div className='gpt3__brand'>
+        <img src={Dropbox} alt='dropbox' />
+      </div>
+      <div className='gpt3__brand'>
+        <img src={Shopify} alt='shopify' />
+      </div>
     </div>
   )
 }
