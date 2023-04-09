a616229bfb52975b57ba2e77c3fa35489235c2d0
Andy Short <automation.andy101@gmail.com>
2023-04-09 12:23:46 +0100

    Completed possibilties component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ import Logo from '../../assets/logo.svg';
 const Menu = () => (
   <>
     <p><a href='#home'>Home</a></p>
-    <p><a href='#Wgpt3'>What is GPT3?</a></p>
+    <p><a href='#whpt3'>What is GPT3?</a></p>
     <p><a href='#possibility'>Open AI</a></p>
     <p><a href='#features'>Case Studies</a></p>
     <p><a href='#blog'>Library</a></p>
