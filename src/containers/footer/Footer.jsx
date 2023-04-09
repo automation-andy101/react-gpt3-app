171775507e6c69946a9bfd6c16d07fb7bae5cfc9
Andy Short <automation.andy101@gmail.com>
2023-04-09 16:46:19 +0100

    Finished app

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
 import './footer.css';
+import Gpt3Logo from '../../assets/logo.svg';
 
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className="gradient__text">Do you want to step into the future before others</h1>
+      </div>
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={Gpt3Logo} alt='' />
+          <p>Automation101 <br /> All Rights Reserved</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>31 Nowhere Street, Nowhere Land</p>
+          <p>818-123456</p>
+          <p>automation101@info.net</p>
+        </div>
+      </div>
+
+      <div className="gpt3__footer-copyright">
+        <p>&copy; 2023 GPT-3. All rights reserved.</p>
+      </div>
     </div>
   )
 }
